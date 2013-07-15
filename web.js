02b8acae59b7fc8a9d122a4383ca01c304a8da9d
Anupam Mittal <anupam@ubuntu.(none)>
2013-07-15 18:43:47 +0530

    testing

--- web.js
+++ web.js
@@ -17,5 +17,5 @@ fs.readFile('index.html', function (err, data) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  //console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
