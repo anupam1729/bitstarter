0e07d17b1fc9628e129b883a45cdf0b51bfcba47
Anupam Mittal <anupam@ubuntu.(none)>
2013-07-18 20:42:45 +0530

    GetICICIQuote

---
+++ GetICICIQuotesBulk.js
@@ -0,0 +1,31 @@
+var iciciquote = require("./GetICICIQuote");
+var fs = require('fs');
+var async = require('async');
+var moment = require('moment');
+
+var finalCSVString ="";
+var timeNow = moment();
+//var symbol = process.argv[2];
+//var expiryDate = process.argv[3];
+//if (symbol == null) symbol = "HCLTEC";
+//if (expiryDate == null) expiryDate = "25-Jul-2013";
+
+var fs = require('fs');
+var symbolsArray = fs.readFileSync('futureslist.tls').toString().split("\r\n");
+
+async.forEach(symbolsArray,
+    function (symbol, callback) {
+        //console.log("!!!" + symbol);
+        iciciquote.returnQuoteJSON(symbol, function (foo) {
+            finalCSVString += foo.Symbol + "," + (moment(foo.LastTradeDate, 'DD-MMM-YYYY')).format('YYYYMMDD') + "," + foo.DayOpen + "," + foo.DayHigh + "," + foo.DayLow + "," + foo.LastTradePrice + "," + foo.Volume + "," + foo.PrevDayClose + "\r\n";
+            callback();
+        });
+    },
+     function () {
+         //var jsonString = JSON.stringify(foo);
+         //console.log("..." + finalCSVString);
+         fs.writeFile("test.csv", finalCSVString);
+         console.log("took " + moment().diff(timeNow));
+     }
+);
+
