740d299ca57db504b4f79fb824d06ac525b4dc4a
Anupam Mittal <anupam@ubuntu.(none)>
2013-07-19 10:38:38 +0530

    Quote

--- web.js
+++ web.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var iciciquote = require("./GetICICIQuote");
+var fs = require('fs');
+var async = require('async');
+var moment = require('moment');
+
 
 var app = express.createServer(express.logger());
 
@@ -16,7 +21,32 @@ app.get('/', function (request, response) {
 
 app.get('/Quote', function (request, response) {
 
-    response.send('work in progress');
+    var finalCSVString = "";
+    var timeNow = moment();
+    //var symbol = process.argv[2];
+    //var expiryDate = process.argv[3];
+    //if (symbol == null) symbol = "HCLTEC";
+    //if (expiryDate == null) expiryDate = "25-Jul-2013";
+
+    var fs = require('fs');
+    var symbolsArray = fs.readFileSync('futureslist.tls').toString().split("\r\n");
+
+    async.forEach(symbolsArray,
+    function (symbol, callback) {
+        //console.log("!!!" + symbol);
+        iciciquote.returnQuoteJSON(symbol, function (foo) {
+            finalCSVString += foo.Symbol + "," + (moment(foo.LastTradeDate, 'DD-MMM-YYYY')).format('YYYYMMDD') + "," + foo.DayOpen + "," + foo.DayHigh + "," + foo.DayLow + "," + foo.LastTradePrice + "," + foo.Volume + "," + foo.PrevDayClose + "\r\n";
+            callback();
+        });
+    },
+     function () {
+         //var jsonString = JSON.stringify(foo);
+         //console.log("..." + finalCSVString);
+         //fs.writeFile("test.csv", finalCSVString);
+         //response.send(test.csv);
+         response.send(finalCSVString);
+     }
+);
 });
 
 var port = process.env.PORT || 5000;
